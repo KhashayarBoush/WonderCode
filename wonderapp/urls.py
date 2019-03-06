e9c8eb8126a9181155e62dddf49f1b65d0e26254
khashiboush <khashayar.boush@icloud.com>
2019-03-07 01:24:20 +0330

    v beta 0.2 home page urls

---
+++ wonderapp/urls.py
@@ -0,0 +1,6 @@
+rom django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home')
+]
--- wonderapp/views.py
+++ wonderapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+def home(request):
+    return render(request, 'wonderapp/home.html', {})
